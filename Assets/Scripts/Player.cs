b09da4745643072b73987d1a05f51f9bef80223a
BornaBazianec <85340274+BornaBazianec@users.noreply.github.com>
2022-04-01 09:34:37 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rigidbodyComponent;
+    bool jump;
+    bool touchGround;
+    private float horizontal;
+    public AudioClip coinSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+        jump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&& touchGround==true)
+        {
+            jump = true;
+        }
+        horizontal = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        if (jump)
+        {
+            rigidbodyComponent.AddForce(6 * Vector3.up,ForceMode.VelocityChange);
+            jump = false;
+            touchGround = false;
+        }
+        rigidbodyComponent.velocity = new Vector3(horizontal, rigidbodyComponent.velocity.y, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        touchGround = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        AudioSource.PlayClipAtPoint(coinSound, transform.position);
+    }
+}
